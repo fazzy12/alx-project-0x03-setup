81a568219175b4d4e80b4979210646f97833732d
ifeanyi kalu <kaluifeanyi00@gmail.com>
2025-09-27 02:19:14 +0100

    Move your interfaces to the right directory

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -2,7 +2,6 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { LayoutProps } from "../interface";
 
-
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
